4dcd4a35c3580342df919acd4decc339474f8407
Kevin Figuera <kevinalejandro3@gmail.com>
2021-12-04 10:56:10 -0400

    Arrow Functions, Promesas y Parámetros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,4 @@
+/*
 //Anterior a ES6
 function newFunction(name, age, country) {
     var name = name || 'oscar';
@@ -46,3 +47,48 @@ let team2 = ['Valeria', 'Yesica', 'Camila'];
 let education = ['David',...team1,...team2];
 
 console.log(education);
+*/
+
+let name = 'oscar';
+let age = 32;
+//es5
+obj = {name: name, age: age};
+//es6
+obj2 = {name, age};
+
+console.log(obj, obj2)
+
+//Arrow Function
+const names = [
+    {name: 'Oscar', age: 32},
+    {name: 'Kevin', age: 27}
+];
+let listOfNames = names.map(function (item) {
+        console.log(item.name);
+    }
+);
+
+let listOfNames2 = names.map((item) => console.log(item.name));
+const listOfNames3 = (name, age, country) => {
+    console.log(name);
+}
+
+const listOfNames4 = name => {
+    console.log(name);
+}
+
+const square = num => num * num;
+
+//Promise
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+       if(false){
+           resolve('Todo Ok');
+       } else{
+           reject('Algo salio mal');
+       }
+    });
+}
+helloPromise()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
