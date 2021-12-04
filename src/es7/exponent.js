b6732ef23b4899713c63d91ee773344010c7eda4
Kevin Figuera <kevinalejandro3@gmail.com>
2021-12-04 11:59:09 -0400

    ES7

---
+++ src/es7/exponent.js
@@ -0,0 +1,4 @@
+let base = 4;
+let exponent = 1;
+let result = base ** exponent;
+console.log(result);
---
+++ src/es7/includes.js
@@ -0,0 +1,7 @@
+//Include trabaja sobre un string
+let numbers = [1, 2, 3, 7, 8];
+if(numbers.includes(5)){
+    console.log('si se encuentra el valor 7');
+}else{
+    console.log('no se encuentra');
+}
